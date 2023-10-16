9cf18de0993cf0b114cde16eeb3a7d42dc33bc94
Kelvin Mue <kelvinmue@gmail.com>
2023-10-16 17:37:01 +0300

    Completed assignment

--- server/app.py
+++ server/app.py
@@ -1,34 +1,49 @@
 #!/usr/bin/env python3
 
-from flask import Flask, make_response
-from flask_migrate import Migrate
+from random import choice as rc
 
-from models import db, Zookeeper, Enclosure, Animal
+from faker import Faker
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+from app import app
+from models import db, Zookeeper, Animal, Enclosure
 
-migrate = Migrate(app, db)
+fake = Faker()
 
-db.init_app(app)
+with app.app_context():
 
-@app.route('/')
-def home():
-    return '<h1>Zoo app</h1>'
+    Animal.query.delete()
+    Zookeeper.query.delete()
+    Enclosure.query.delete()
 
-@app.route('/animal/<int:id>')
-def animal_by_id(id):
-    return ''
+    zookeepers = []
+    for n in range(25):
+        zk = Zookeeper(name=fake.name(), birthday=fake.date_between(
+            start_date='-70y', end_date='-18y'))
+        zookeepers.append(zk)
 
-@app.route('/zookeeper/<int:id>')
-def zookeeper_by_id(id):
-    return ''
+    db.session.add_all(zookeepers)
 
-@app.route('/enclosure/<int:id>')
-def enclosure_by_id(id):
-    return ''
+    enclosures = []
+    environments = ['Desert', 'Pond', 'Ocean', 'Field', 'Trees', 'Cave', 'Cage']
 
+    for n in range(25):
+        e = Enclosure(environment=rc(environments), open_to_visitors=rc([True, False]))
+        enclosures.append(e)
 
-if __name__ == '__main__':
-    app.run(port=5555, debug=True)
+    db.session.add_all(enclosures)
+
+    animals = []
+    species = ['Lion', 'Tiger', 'Bear', 'Hippo', 'Rhino', 'Elephant', 'Ostrich',
+        'Snake', 'Monkey']
+
+    for n in range(200):
+        name = fake.first_name()
+        while name in [a.name for a in animals]:
+            name=fake.first_name()
+        a = Animal(name=name, species=rc(species))
+        a.zookeeper = rc(zookeepers)
+        a.enclosure = rc(enclosures)
+        animals.append(a)
+
+    db.session.add_all(animals)
+    db.session.commit()
